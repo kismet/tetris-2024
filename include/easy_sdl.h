d9d734365e16617c2dd9bf569ae410957eb4460e
Stefano Lenzi <stefano@lenzi.pro>
2024-03-06 23:56:01 +0100

    Preparing Alpha version of Easy_SDL API Initial implementation of initEasySDL, freeEasySDL, getSDLWindow, getSDLRender, loadImage

--- include/easy_sdl.h
+++ include/easy_sdl.h
@@ -3,10 +3,94 @@
 
 #include <SDL.h>
 
+#define EASY_SDL_DEFAULT_ASSET_SLOT 50
+#define EASY_SDL_DEFAULT_WINDOW_HEIGHT 768
+#define EASY_SDL_DEFAULT_WINDOW_WIDTH 1366
+#define EASY_SDL_DEFAULT_WINDOW_OPTIONS SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN
 
-SDL_Window* getSDLWindow();
 
-SDL_Renderer* getSDLRender();
+typedef struct Image_Asset {
+    uint16_t height;
+    uint16_t width;
+    SDL_Texture* texture;
+    SDL_Surface* surface;
+} Image_Asset_t;
+
+typedef struct Font_Asset {
+    bool placeholder;
+} Font_Asset_t;
+
+typedef enum Asset_Type{
+    ASSET_IMAGE, ASSET_FONT
+} Asset_Type_t;
+
+typedef union Easy_Asset_Union {
+    Image_Asset_t image;
+    Font_Asset_t font;
+} Easy_Asset_Union_t;
+
+
+typedef struct Easy_Asset {
+    Asset_Type_t type;
+    uint32_t id;
+    bool loaded;
+    char* origin;
+    char* name;
+    Easy_Asset_Union_t detail;
+} Easy_Asset_t;
+
+/**
+ * This is the <b>FIRST</b> that must invoked by the developers
+ * for creating a proper context, otherwise all the other function
+ * will fail
+ * By default this crate a FRAMED window with Hardware acceleration
+ * enabled, moreover this function will load all the SDL most common
+ * SDL subsystem (Core, Image, and Font)
+ *
+ * @param title The title to assign to the widow
+ * @param width Width of the window
+ * @param height Height of the window
+ * @param options other options to pass to SDL please refers to #SDL_init()
+ * @return TRUE if and only if the initialization succeeded
+ */
+bool initEasySDL(char* title, int width, int height, uint32_t options );
+
+/**
+ * This load a 800x600 windows with "Easy SDL" as title
+ * by invoking the initEasySDL function
+ *
+ * @see initEasySDL
+ */
+bool initEasySDL();
+
+bool freeEasySDL();
+
+// TODO lazy loading assest
+// Easy_Asset_t* addAsset(char* path);
+
+Easy_Asset_t* loadAsset(char* path);
+
+/**
+ * This function returns a loaded Asset (eventually it will try to load it)
+ *
+ * @param id the id that represents the asset to require
+ * @return the Asset matching the id or NULL if none is found
+ */
+Easy_Asset_t* getAssetById(int id);
+
+//
+//Easy_Asset_t* getAssetById(char* name);
+
+/**
+ * Load a Font as Assets and return it, otherwise it will delete
+ *
+ *
+ * @param path
+ * @return
+ */
+Easy_Asset_t* loadFont(char* path);
+
+Easy_Asset_t* loadImage(char* path);
 
 void drawAsset(uint16_t x, uint16_t y, SDL_Texture *asset, uint16_t rotation, float scaling);
 
@@ -14,6 +98,19 @@ void drawAsset(uint16_t x, uint16_t y, SDL_Texture *asset, uint16_t rotation);
 
 void drawAsset(uint16_t x, uint16_t y, SDL_Texture *asset);
 
-bool initEasySDL(char* title, uint16_t width, uint16_t height, uint16_t options );
+/**
+ * The following section contains function that allows the developer
+ * to access the SDL layer for using specific function that are not
+ * exposed by the EasySDL layer
+ */
+
+/**
+ *
+ * @return
+ */
+SDL_Window* getSDLWindow();
+
+SDL_Renderer* getSDLRender();
+
 
 #endif //EASY_SDL_H
--- src/easy_sdl.cpp
+++ src/easy_sdl.cpp
@@ -6,59 +6,168 @@
 
 using namespace std;
 
-static SDL_Renderer* mainRenderer = NULL;
-static SDL_Window* window = NULL;
+typedef struct Easy_SDL_Context {
+    SDL_Renderer* renderer = NULL;
+    SDL_Window* window = NULL;
+    bool subsystem_sdl_loaded = false;
+    bool subsystem_image_loaded = false;
+    bool subsystem_ttf_loaded = false;
+    uint16_t n_assets = 0;
+    uint16_t max_assets = 0;
+    Easy_Asset_t* assets = NULL;
+} Easy_SDL_Context_t;
+
+static Easy_SDL_Context_t context;
+
 
 SDL_Window* getSDLWindow(){
-    return window;
+    return context.window;
 }
 
 SDL_Renderer* getSDLRender() {
-    return mainRenderer;
+    return context.renderer;
 }
 
-bool initEasySDL(char* title, int width, int height, int options ){
+bool freeEasySDL() {
+    if (!(context.subsystem_sdl_loaded
+        || context.subsystem_image_loaded
+        || context.subsystem_ttf_loaded ) ) {
+        return true;
+    }
+    if ( context.renderer != NULL ) {
+        //TODO Check error cleaning
+        SDL_RenderClear( context.renderer );
+    }
+    if ( context.window != NULL ) {
+        //TODO Check error cleaning
+        SDL_DestroyWindow( context.window );
+    }
+    //TODO delete all the loaded asssets
+
+    if (context.subsystem_ttf_loaded) {
+        //TODO Check error cleaning
+        TTF_Quit();
+    }
+    if (context.subsystem_image_loaded) {
+        //TODO Check error cleaning
+        IMG_Quit();
+    }
+    if (context.subsystem_sdl_loaded) {
+        //TODO Check error cleaning
+        SDL_Quit();
+    }
+    return true;
+}
+bool initEasySDL( ) {
+    return initEasySDL("EasySDL",
+                       (int) EASY_SDL_DEFAULT_WINDOW_HEIGHT,
+                       (int) EASY_SDL_DEFAULT_WINDOW_WIDTH,
+                       (uint32_t) EASY_SDL_DEFAULT_WINDOW_OPTIONS
+    );
+}
+
+
+bool initEasySDL(char* title, int width, int height, uint32_t options ){
+    if ( context.subsystem_sdl_loaded ){
+        return context.subsystem_sdl_loaded;
+    }
     //TODO Free SDL, TTF, and IMG on failure
     if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) {
         cerr<< "Error initializing SDL: " << SDL_GetError() << endl;
         return false;
+    } else {
+        context.subsystem_sdl_loaded = true;
     }
 
     if ( TTF_Init() < 0 ){
         cerr << "Error initializing TTF Failed: " << TTF_GetError() << endl;
         return false;
+    } else {
+        context.subsystem_ttf_loaded = true;
     }
 
     if ( IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) < 0 ) {
         cerr << "Error initializing SDL Image: " << IMG_GetError() << endl;
         return false;
-    };
-
+    } else {
+        context.subsystem_image_loaded = true;
+    }
+    SDL_Window* w = NULL;
     if (options == 0 )  {
-        window = SDL_CreateWindow( title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        w = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                    height, width, SDL_WINDOW_RESIZABLE );
     }else{
-        window = SDL_CreateWindow( title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        w = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                    height, width, options );
     }
-    if ( !window ) {
+    if ( !w ) {
         cerr << "Error creating window: " << SDL_GetError()  << endl;
         return false;
+    }else{
+        context.window = w;
     }
 
-    mainRenderer = SDL_CreateRenderer( window, -1 , SDL_RENDERER_ACCELERATED );
-    if ( !mainRenderer ) {
+    SDL_Renderer* r = NULL;
+    r = SDL_CreateRenderer( w, -1 , SDL_RENDERER_ACCELERATED );
+    if ( !r ) {
         cerr << "Error getting renderer: " << SDL_GetError() << endl;
         return false;
+    }else{
+        context.renderer =  r;
     }
-/* Cleaning
-    SDL_RenderClear( mainRenderer );
-    SDL_DestroyWindow( window );
-    SDL_Quit();
-*/
-    return true;
+    if (context.subsystem_sdl_loaded
+    && context.subsystem_image_loaded
+    && context.subsystem_ttf_loaded ) {
+        context.max_assets = EASY_SDL_DEFAULT_ASSET_SLOT;
+        context.assets = (Easy_Asset_t*) malloc(sizeof(Easy_Asset_t)*context.max_assets);
+        context.n_assets = 0;
+        return true;
+    }else{
+        //TODO Clean partial loaded SDL
+        return false;
+    }
 }
 
+Easy_Asset_t* loadFont(char* path){
+    return NULL;
+}
 
+Easy_Asset_t* loadImage(char* path){
+    if ( context.renderer == NULL ) {
+        cerr << "No valid render. Assets cannot be loaded! Please invoke ??? first" << endl;
+        return NULL;
+    }
+
+    if( context.n_assets == context.max_assets ){
+        //TODO increase number of assets
+        cerr<<"No more space for loading assets. Dynamic assets memories will fixed in next release"<<endl;
+        return NULL;
+    }
+    Easy_Asset_t* asset = &context.assets[context.n_assets];
+
+    SDL_Surface* image = SDL_LoadBMP(path);
+    if(!image){
+        cerr<<"Unable to load "<<path<<" "<<SDL_GetError();
+        return NULL;
+    }
+    asset->detail.image.surface = image;
+    asset->detail.image.height = image->h;
+    asset->detail.image.width = image->w;
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(
+            context.renderer, image );
+    if(!texture){
+        cerr<<"Unable to create texture "<<path<<" "<<SDL_GetError();
+        return NULL;
+    }
+    asset->detail.image.texture = texture;
+    asset->type = ASSET_IMAGE;
+    asset->id = context.n_assets;
+    asset->origin = (char *) malloc(strlen(path));
+    strcpy(asset->origin,path);
+    context.n_assets++;
+    //TODO init name
+    return asset;
+}
 
 
