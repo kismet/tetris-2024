88011dbdeb6305b5fc1e44862360de75642d4e1e
Stefano Lenzi <stefano@lenzi.pro>
2024-04-14 12:21:19 +0200

    CLEAN UP code after porting to EasySDL

--- main.cpp
+++ main.cpp
@@ -12,15 +12,6 @@
 
 using namespace std;
 
-static int cx = 0;
-static int cy = 0;
-static unsigned int idxBlock = 0;
-static int angle = 0;
-
-static int score = 0;
-
-static int SPRITE_WIDTH = 256;
-static int SPRITE_HEIGHT = 256;
 
 static int MaxFallSpeed = 1000;
 
@@ -69,12 +60,6 @@ static char world[WORLD_HEIGHT][WORLD_WIDTH];
 const int SCREEN_WIDTH = 1400;
 const int SCREEN_HEIGHT = 938;
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-TTF_Font* font = NULL;
-TTF_Font* fontRules = NULL;
-SDL_Texture* backgroundTexture = NULL;
-
 const int MENU_OPTIONS_COUNT = 3;
 char* MENU_OPTIONS[MENU_OPTIONS_COUNT] = {"Start Game", "Resume", "Quit"};
 
@@ -83,7 +68,6 @@ int selectedOption = 0;
 void menu(SDL_Event*);
 void game(SDL_Event*);
 
-bool loadAssets(SDL_Renderer* render);
 void (*gestore_eventi)(SDL_Event *) = &menu;
 
 
@@ -104,94 +88,9 @@ char* assetsOrigin[]  = {
 Easy_Asset_t* assets[sizeof(assetsOrigin)/sizeof(char*)];
 
 
-
-bool initSDL() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        cout << "SDL initialization failed: " << SDL_GetError() << endl;
-        return false;
-    }
-
-    window = SDL_CreateWindow(
-                              "Tetris - 3AI",
-                              SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED,
-                              SCREEN_WIDTH, SCREEN_HEIGHT,
-                              SDL_WINDOW_SHOWN);
-
-    if (window == NULL) {
-        cout << "Window creation failed: " << SDL_GetError() << endl;
-        return false;
-    }
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        cout << "Renderer creation failed: " << SDL_GetError() << endl;
-        return false;
-    }
-
-    if (TTF_Init() == -1) {
-        cout << "SDL_ttf initialization failed: " << TTF_GetError() << endl;
-        return false;
-    }
-
-    font = TTF_OpenFont("assets/fonts/ka1.ttf", 42);
-    fontRules = TTF_OpenFont("assets/fonts/Noteworthy.ttc", 48);
-
-    if(fontRules == NULL) {
-        cout << "Font loading failded: " << TTF_GetError() << endl;
-        return false;
-    }
-
-    if (font == NULL) {
-        cout << "Font loading failed: " << TTF_GetError() << endl;
-        return false;
-    }
-
-    return true;
-}
-
-void closeSDL() {
-    SDL_DestroyTexture(backgroundTexture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_CloseFont(font);
-    TTF_Quit();
-    SDL_Quit();
-}
-
-SDL_Texture* loadTexture(const string& filePath) {
-    SDL_Surface* surface = IMG_Load(filePath.c_str());
-    if (surface == NULL) {
-        cout << "Unable to load image " << filePath << ". Error: " << IMG_GetError() << endl;
-        return NULL;
-    }
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-    return texture;
-}
-
-void renderText(const string& text, int x, int y, bool selected = false) {
-    return ;
-    SDL_Color color = {255, 255, 255};
-    if (selected) {
-        color = {255, 0, 0};
-    }
-    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-
-    int width = surface->w;
-    int height = surface->h;
-    SDL_FreeSurface(surface);
-
-    SDL_Rect destRect = {x - width / 2, y - height / 2, width, height};
-    SDL_RenderCopy(renderer, texture, NULL, &destRect);
-
-    SDL_DestroyTexture(texture);
-}
-
 void drawMenu() {
     // Clear the screen
-    renderer = getSDLRender();
+    SDL_Renderer *renderer = getSDLRender();
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
@@ -215,7 +114,6 @@ void drawMenu() {
                 (uint16_t ) SCREEN_WIDTH, (uint16_t ) 50,
                 MENU_OPTIONS[i], TEXT_CENTERED
         );
-        //renderText(MENU_OPTIONS[i], x, y, isSelected);
     }
 
     // Update the screen
@@ -285,26 +183,12 @@ void menu(SDL_Event* e){
 
 
 int main(int argc, char** args) {
-    /*
-    if (!initSDL()) {
-        cout << "SDL initialization failed!" << endl;
-        return 1;
-    }
-
-    if (backgroundTexture == NULL) {
-        closeSDL();
-        return 1;
-    }
-    */
     bool quit = false;
 
     initEasySDL("Tetris 2024", SCREEN_WIDTH , SCREEN_HEIGHT,
                 (uint32_t) EASY_SDL_DEFAULT_WINDOW_OPTIONS);
-    window = getSDLWindow();
-    renderer = getSDLRender();
     normal.font = loadAsset("assets/fonts/ka1.ttf");
     highlight.font = normal.font;
-    backgroundTexture = loadTexture("assets/grafica/pausa_new_1.png");
     gestore_eventi =  &menu;
 
     while (!quit) {
